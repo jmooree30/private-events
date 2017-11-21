0a06564ae2e8c1460f61f77168ee9fd211a025a4
Jacob <jmooree30@gmail.com>
2017-11-20 23:32:12 -0500

    associations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :events, :foreign_key => "creator_id"
     has_many :attendances
     has_many :attended_events, through: :attendances, :source => "event"
+
+ 
 end
